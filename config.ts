babb5b45f9364942dec57e5314fadef49ff74cd3
juanpabloct <juanptorres2003@gmail.com>
2023-06-16 19:48:04 -0500

    Llaves de configuracion localStorage

--- config.ts
+++ config.ts
@@ -1,1 +1,8 @@
 export const { VITE_TOKEN_MAPBOX, VITE_ROUTE_BACK } = import.meta.env;
+
+enum KeyLocalStorage {
+  token = "token",
+  userId = "userId",
+  role = "role",
+}
+export const { token, userId, role } = KeyLocalStorage;
