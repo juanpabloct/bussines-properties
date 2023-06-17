c9dc4bb6fb7271ff1620f50aac5dae3a373bbf51
juanpabloct <juanptorres2003@gmail.com>
2023-06-16 19:50:02 -0500

    Rutas de la pagina

---
+++ src/common/routes.tsx
@@ -0,0 +1,41 @@
+import { lazy } from "react";
+import { Loading } from "../components/loading/loading";
+
+// eslint-disable-next-line react-refresh/only-export-components
+const LoginPage = lazy(() => import("../pages/login.page"));
+// eslint-disable-next-line react-refresh/only-export-components
+const HomePage = lazy(() => import("../pages/map.page"));
+// eslint-disable-next-line react-refresh/only-export-components
+const RegisterForm = lazy(() => import("../pages/register.pages"));
+// eslint-disable-next-line react-refresh/only-export-components
+const AdminRegister = lazy(() => import("../pages/registerAdmin.pages"));
+enum allRoute {
+  login = "/",
+  home = "/map",
+  register = "/register",
+  adminRegister = "/adminRegister",
+  error = "*",
+}
+export const { home, error, login, register, adminRegister } = allRoute;
+export const routes = [
+  {
+    route: home,
+    validToken: { valid: true },
+    element: HomePage,
+  },
+  {
+    validToken: { redirect: home },
+    route: login,
+    element: LoginPage,
+  },
+  {
+    validToken: { redirect: home },
+    route: adminRegister,
+    element: AdminRegister,
+  },
+  {
+    route: register,
+    element: RegisterForm,
+  },
+  { route: error, element: Loading },
+];
