812c91d22ca3bcfbe21bda653a5d3c323becfd61
juanpabloct <juanptorres2003@gmail.com>
2023-06-16 19:49:52 -0500

    Movio strictMode a un componente debajo

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { AllRoutes } from "./pages/AllRoutes";
+import { Allroutes } from "./pages/AllRoutes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    {
-      <Router>
-        <AllRoutes />
-      </Router>
-    }
-  </React.StrictMode>
+  <Router>
+    <Allroutes />
+  </Router>
 );
